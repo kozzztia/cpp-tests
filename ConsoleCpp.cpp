1034d0ef6d6ced5b2fd7737434401758dc660759
Kostia <kozzztia@gmail.com>
2026-04-19 12:32:25 +0300

    setlocale(LC_ALL, "UA") for  using  language

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 int main() {
+    setlocale(LC_ALL, "UA");
     std::cout << "Hello, C++!\n";
     std::cout << "version 1.9.1\n";
+    std::cout << L"текст  і Ї\n";
     return 0;
 }
