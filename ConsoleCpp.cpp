0730781064003b8999c50e9ec95ee394b209dd03
Kostia <kozzztia@gmail.com>
2026-04-20 13:39:31 +0300

    fload  7  numbers double 15numbers

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+// #include <iomanip>
 
 int main() {
     // date  type
@@ -8,10 +9,11 @@ int main() {
     short short_number = 2; // short integer  -32k /+32k 2bits
     long long_number = 1000000000; // long integer -9 quintillion / +9 quintillion 4bits windows 8 linux
     long long long_long_number = 1000000000000; // long long integer -9 quintillion / +9 quintillion 8bits
-    float float_number = 3.141592653589793f; // floating point number 4bits f as good practice
-    double double_number = 3.141592653589793; // double precision floating point number 8bits
+    float float_number = 3.1415926f; // floating point number 4bits f as good practice 7 digits precision
+    double double_number = 3.141592653589793; // double precision floating point number 8bits 15 digits precision
 
 
+    // std::cout << std::setprecision(10); // установить точность вывода
     std::cout << "float: " << float_number << std::endl;
     std::cout << "double: " << double_number << std::endl;
 
