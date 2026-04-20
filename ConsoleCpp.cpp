82f0cb51f8bef6b69a84cbe91b211c688556b1d3
Kostia <kozzztia@gmail.com>
2026-04-20 20:12:39 +0300

    string in c++

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
+#include <cstring> 
 
 int main() {
 
-    char text; 
+    // more safe
+    std::string text;
 
     std::cout << "Enter a text: ";
     std::cin >> text;
 
-    switch (text)
+    if(text == "one")
     {
-    case '1' :
-        std::cout << "Number is: one" << std::endl;
-        break;
-    case '2':
-        std::cout << "Number is: two" << std::endl;
-        break;
-    case '3' :
-        std::cout << "Number is: three" << std::endl;
-        break;
-    default:
-        std::cout << "you lose" << std::endl;
-        break;
+        std::cout << "1";
+    }else if (text == "two")
+    {
+        std::cout << "2";
     }
+    else if (text == "three")
+    {
+        std::cout << "three";
+    }
+    else
+    {
+        std::cout << "other";
+    }
+
+    // old variant
+    // char text[10]; 
+
+    // std::cout << "Enter a text: ";
+    // std::cin >> text;
+
+    // if(std::strcmp(text, "one") == 0)
+    // {
+    //     std::cout << "1";
+    // }else if (std::strcmp(text, "two") == 0)
+    // {
+    //     std::cout << "2";
+    // }
+    // else if (std::strcmp(text, "three") == 0)
+    // {
+    //     std::cout << "three";
+    // }
+    // else
+    // {
+    //     std::cout << "other";
+    // }
 
     return 0;
 }
