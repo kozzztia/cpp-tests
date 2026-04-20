347af3da4be7810b26ce64d41f6855ead7f4bfde
Kostia <kozzztia@gmail.com>
2026-04-20 13:07:43 +0300

    minimal calc

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
 int main() {
-    int a, b, c;
+    int a, b, c , result;
+    char aa[] = "insert a: ", bb[] = "insert b: ", cc[] = "insert c: ";
 
-    std::cin >> a >> b >> c;
+    std::cout << aa;
+    std::cin >> a;
+    std::cout << bb;
+    std::cin >> b;
+    std::cout << cc;
+    std::cin >> c;
+
+    result = a + b + c;
+
+    std::cout << "result" << result << std::endl;
 
     return 0;
 }
