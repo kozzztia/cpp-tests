b81d294b5b5abf39a05830b23d421d98a25fa69f
Kostia <kozzztia@gmail.com>
2026-04-20 20:26:25 +0300

    using  boolean

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -5,12 +5,19 @@ int main() {
 
     // more safe
     std::string text;
+    std::string flag_input;
+    bool flag = false; 
 
     std::cout << "Enter a text: ";
     std::cin >> text;
+    std::cout << "Enter a flag  (true/false): ";
+    std::cin >> flag;
 
-    if(text == "one") std::cout << "1";
-    else if (text == "two") std::cout << "2";
+    if (flag_input == "true") flag = true;
+    else flag = false;
+
+    if(text == "one" || text == "four") std::cout << "1 or  4";
+    else if (text == "two" && flag) std::cout << "2 and flag  true";
     else if (text == "three") std::cout << "three";
     else std::cout << "other";
 
