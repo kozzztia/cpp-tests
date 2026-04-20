84aedc1727665df2ad5c24f3f0472e35ab54abdb
Kostia <kozzztia@gmail.com>
2026-04-20 19:22:29 +0300

    switch case

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
-#include <cstdlib>   // для rand() и srand()
-#include <ctime>     // для time()
 
 int main() {
 
-    std::srand(std::time(nullptr));
-    int random_number = 1 + std::rand() % 10;
-    int count = 0;
     int number; 
 
-    while (true)
+    std::cout << "Enter a number (1 -4): ";
+    std::cin >> number;
+
+    switch (number)
     {
-        std::cout << "Enter a number (1 -10): ";
-        std::cin >> number;
-        if (number == random_number) {
-            std::cout << "You win! Number is: " << number << " you have " << count << " try" << std::endl;
-            break; 
-        } else if (number > random_number) {
-            std::cout << "Less than " << number << std::endl;
-            count += 1;
-        } else {
-            std::cout << "More than " << number << std::endl;
-            count += 1;
-        }
+    case 1 :
+        std::cout << "Number is: one" << std::endl;
+        break;
+    case 2:
+        std::cout << "Number is: two" << std::endl;
+        break;
+    case 3 :
+        std::cout << "Number is: three" << std::endl;
+        break;
+    default:
+        std::cout << "you lose" << std::endl;
+        break;
     }
+
     return 0;
 }
