8f6cbc2e67badff2aaae79c5daf9757ff6bbecdc
Kostia <kozzztia@gmail.com>
2026-04-21 17:41:43 +0300

    iteration for  while  do while

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -1,77 +1,45 @@
 #include <iostream>
-#include <cstring> 
+#include <time.h> 
 
 using namespace std;
 
 
 int main() {
 
-    float num1, num2;
-    char operation;
-    srand(time(NULL));
+    // for 
 
-    // while (true)
-    // {
-    //     std::cout << "insert first number : ";
-    //     if(!(std::cin >> num1)){
-    //         std::cin.clear();
-    //         std::cin.ignore(10000, '\n');
-    //         std::cout << "insert first number as (1,2...): ";
-    //     }else{
-    //         break;
+    // for(int i = 1; i <= 10; i++){
+    //     for(int o = 1; o <= i; o++){
+    //         cout << o ;
     //     }
+    //     cout << endl;
     // }
 
-    // while (true)
-    // {
-    //     std::cout << "insert second number : ";
-    //     if(!(std::cin >> num2)){
-    //         std::cin.clear();
-    //         std::cin.ignore(10000, '\n');
-    //         std::cout << "insert second number as (1,2...): ";
-    //     }else{
-    //         break;
+    // while
+
+    int num = 100, num2 = 100;
+
+    // while(num < 10){
+    //     num++;
+    //     while(num2 < num){
+    //         num2++;
+    //         cout << num2;
     //     }
+    //     num2 = 0;
+    //     cout << endl;
     // }
 
-    // while (true)
-    // {
-    //     std::cout << "insert operation (+, -, *, /): ";
-    //     std::cin >> operation;
-    //     if(operation == '+' || operation == '-' || operation == '*' || operation == '/'){
-    //         break;
-    //     }else{
-    //         std::cin.clear();
-    //         std::cin.ignore(10000, '\n');
-    //     }
-    //     std::cout << "try  again";
-    // }
+    do {
+        num++;
+        do{
+            num2++;
+            cout << num2;
+        }while(num2 < num);
+        num2 = 0;
+        cout << endl;
+    } while (num < 10);
 
-    // switch (operation)
-    // {
-    // case '+' : std::cout << "result is : " <<  num1 + num2;
-    //     break;
-    // case '-' : std::cout << "result is : " <<  num1 - num2;
-    //     break;
-    // case '*' : std::cout << "result is : " <<  num1 * num2;
-    //     break;
-    // case '/' : std::cout << "result is : " <<  num1 / num2;
-    //     break;
     
-    // default:
-    //     std::cout << "wrong  operation or  number";
-    //     break;
-    // }
-
-    // int a = 2, b = 3, c = 9, res = 5;
-
-    // res--;
-
-    // cout << res;
-
-    float num = 1 + rand() % 50;
-
-    cout << num;
 
     return 0;
 }
