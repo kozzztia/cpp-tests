c44124fcdc2c3429b7ad79c224126da3b654f502
Kostia <kozzztia@gmail.com>
2026-04-21 12:45:32 +0300

    using namespace std;  for  correct  using in file  without  std

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -1,71 +1,72 @@
 #include <iostream>
 #include <cstring> 
 
+using namespace std;
+
 
 int main() {
 
     float num1, num2;
     char operation;
 
+    // while (true)
+    // {
+    //     std::cout << "insert first number : ";
+    //     if(!(std::cin >> num1)){
+    //         std::cin.clear();
+    //         std::cin.ignore(10000, '\n');
+    //         std::cout << "insert first number as (1,2...): ";
+    //     }else{
+    //         break;
+    //     }
+    // }
 
+    // while (true)
+    // {
+    //     std::cout << "insert second number : ";
+    //     if(!(std::cin >> num2)){
+    //         std::cin.clear();
+    //         std::cin.ignore(10000, '\n');
+    //         std::cout << "insert second number as (1,2...): ";
+    //     }else{
+    //         break;
+    //     }
+    // }
 
-    while (true)
-    {
-        std::cout << "insert first number : ";
-        if(!(std::cin >> num1)){
-            std::cin.clear();
-            std::cin.ignore(10000, '\n');
-            std::cout << "insert first number as (1,2...): ";
-        }else{
-            break;
-        }
-    }
+    // while (true)
+    // {
+    //     std::cout << "insert operation (+, -, *, /): ";
+    //     std::cin >> operation;
+    //     if(operation == '+' || operation == '-' || operation == '*' || operation == '/'){
+    //         break;
+    //     }else{
+    //         std::cin.clear();
+    //         std::cin.ignore(10000, '\n');
+    //     }
+    //     std::cout << "try  again";
+    // }
 
-    while (true)
-    {
-        std::cout << "insert second number : ";
-        if(!(std::cin >> num2)){
-            std::cin.clear();
-            std::cin.ignore(10000, '\n');
-            std::cout << "insert second number as (1,2...): ";
-        }else{
-            break;
-        }
-    }
-
-
-    while (true)
-    {
-        std::cout << "insert operation (+, -, *, /): ";
-        std::cin >> operation;
-        if(operation == '+' || operation == '-' || operation == '*' || operation == '/'){
-            break;
-        }else{
-            std::cin.clear();
-            std::cin.ignore(10000, '\n');
-        }
-        std::cout << "try  again";
-    }
-
-    switch (operation)
-    {
-    case '+' :
-            std::cout << "result is : " <<  num1 + num2;
-        break;
-    case '-' :
-            std::cout << "result is : " <<  num1 - num2;
-        break;
-    case '*' :
-            std::cout << "result is : " <<  num1 * num2;
-        break;
-    case '/' :
-            std::cout << "result is : " <<  num1 / num2;
-        break;
+    // switch (operation)
+    // {
+    // case '+' : std::cout << "result is : " <<  num1 + num2;
+    //     break;
+    // case '-' : std::cout << "result is : " <<  num1 - num2;
+    //     break;
+    // case '*' : std::cout << "result is : " <<  num1 * num2;
+    //     break;
+    // case '/' : std::cout << "result is : " <<  num1 / num2;
+    //     break;
     
-    default:
-        std::cout << "wrong  operation or  number";
-        break;
-    }
+    // default:
+    //     std::cout << "wrong  operation or  number";
+    //     break;
+    // }
+
+    int a = 2, b = 3, c = 9, res = 5;
+
+    res--;
+
+    cout << res;
 
     return 0;
     
