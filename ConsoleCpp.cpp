d2560359c036a658f6bd7dca665465d557e18d1c
Kostia <kozzztia@gmail.com>
2026-04-22 16:54:40 +0300

    tuple<int,int,int>  calc(const vector<int>& args);  function  sceleton   for normal  using  function  under  main

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include <algorithm> 
-#include <numeric>  
+#include <algorithm> //min max
+#include <numeric>  //accumulate
 #include <string> 
 #include <cstring>
-#include <vector>
+#include <vector> //universal  array
+#include <tuple> //typing 
 
 
 using namespace std;
@@ -21,12 +22,7 @@ using namespace std;
 //     return {sum, min, max};
 // }
 
-tuple<int,int,int>  calc(const vector<int>& args){
-    int sum = accumulate(args.begin(), args.end(), 0);
-    int min = *min_element(args.begin(), args.end());
-    int max = *max_element(args.begin(), args.end());
-    return {sum, min, max};
-}
+tuple<int,int,int>  calc(const vector<int>& args);
 
 int main() {
     vector<int> nums = {1,2,3,4,5,6,7,8,9, -20, 200,};
@@ -36,6 +32,13 @@ int main() {
     cout << max << " " << min << " " << sum << endl;
 
     return 0;
+}
+
+tuple<int,int,int>  calc(const vector<int>& args){
+    int sum = accumulate(args.begin(), args.end(), 0);
+    int min = *min_element(args.begin(), args.end());
+    int max = *max_element(args.begin(), args.end());
+    return {sum, min, max};
 }
 
 
