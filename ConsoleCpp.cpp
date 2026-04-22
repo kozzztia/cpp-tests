a7482f4e8aabfba8809a395a4742b246206381b0
Kostia <kozzztia@gmail.com>
2026-04-22 10:47:41 +0300

    old  try  to  create  string  with  char[] and   out  with  iteration

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -6,29 +6,13 @@ using namespace std;
 
 
 int main() {
-    // memory
-    // 
-    srand(time(NULL));
 
-    int size = 10; 
-    int* nums = new int[size]; //40 byte
-    int* num = new int;
-    *num = 50;
+    char word[] = {'y', 'n', '4', '3', '0' , 't', 'o'};
+    int size = sizeof(word) / sizeof(word[0]);
 
-    for(int i = 0; i < size; i++){
-        nums[i] = 1 + rand() % 100;
+    for(int i = 0, z = size -1; i < size; i++, z--){
+        cout << word[i] << ' ' << word[z] << endl;
     }
 
-    for(int i = 0; i < size; i++){
-        cout << nums[i];
-        i == size - 1 ? cout << '.' : cout << ", ";
-    }
-    
-    delete[] nums;
-    cout << "there :" << *num << endl;
-    delete num;
-    nums = nullptr;
-    num = nullptr;
-    size = 0;
     return 0;
 }
