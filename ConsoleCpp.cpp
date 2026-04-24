c0bfefb37745199d9cb9a4b0c6017b174e55ec0f
Kostia <kozzztia@gmail.com>
2026-04-24 12:16:25 +0300

    my  try   to catch  throw

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -14,6 +14,8 @@ using namespace std;
 
  void divide(float a , float b){
     if(b == 0) throw 100;
+    if(a == 3) throw 101;
+    if(b == 3) throw 102;
 
     else cout << (a / b) << endl;
  }
@@ -32,8 +34,9 @@ int main() {
     
     catch (int e){
         if(e == 100) cout << "error" << endl;
+        if(e == 101) cout << "a = 3" << endl;
+        if(e == 102) cout << "b = 3" << endl;
     };
-
 
     return 0;
 }
