269e60a00d1a1e9e4120230dd4c643307674e263
Kostia <kozzztia@gmail.com>
2026-04-24 19:07:18 +0300

    ~Building(){             cout << "DELEDE object" << type << endl;         } выполняетьс я  когда  удаляеться  обьект

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -16,14 +16,28 @@ class Building{
         string type;
         string address;
         float cost;
+        int length_result;
 
     public: 
+
         Building(const string&  set_type, const string&  set_address, const float&  set_cost)
             : type(set_type), address(set_address), cost(set_cost)
-        {};
+        {
+           length_result = address.length();
+        };
 
-        void get_info() { cout << "address -" << address << ", type - " << type << endl; };
+        Building()
+            : type("halupa"), address("shlapi 3"), cost(100)
+        {
+           length_result = address.length();
+        };
+
+        void get_info() { cout << "address -" << address << ", type - " << type << " letter length :" << length_result << endl; };
         void get_memory() { cout << "address : " << this << endl; };
+
+        ~Building(){
+            cout << "DELEDE object" << type << endl;
+        }
 };
 
 int main() {
@@ -31,9 +45,13 @@ int main() {
     home.get_info();
     home.get_memory();
 
-    Building school("school", "shlapi 23", 1400);
+    Building school("school", "shlapi 13", 1400);
     school.get_info();
     school.get_memory();
+
+    Building halupa;
+    halupa.get_info();
+    halupa.get_memory();
 
     return 0;
 }
