e06c0dcecb0409615eee86ad955214e05f64f30b
Kostia <kozzztia@gmail.com>
2026-04-25 14:30:44 +0300

    class with  template <class T, size_t N>

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -5,31 +5,76 @@
 
 using namespace std;
 
-template <typename T, typename T2, size_t N>//шаблон с типом и размером.
+template <class T, size_t N>
 
-void show_all(T (&arr)[N]) {// «ссылка на массив из N элементов типа T».
+class Arrays {
+    protected: 
+        // poiner  to  array
+        T* arr;
+    
+    public: 
+        Arrays(T (&a)[N] ){
+            arr = new T[N];
 
-        for(T2 i = 0; i < N; i++ ){
-            cout << arr[i] << " ";//перешагивание  на  елемент  в  масиве
+            for(int i = 0; i < N; i++) 
+                *(arr + i) = *(a + i);
+
+            cout << "start" << endl;;
         };
 
-        cout << endl;
-}
+        void get_all(){
+            for(int i = 0; i < N; i++){
+                cout << *(arr + i);
+                if(i < N -1) cout  << ", ";
+            }
+
+            cout << endl;
+        };
+
+        void get_summ(){
+            T sum = 0;
+            for(int i = 0; i < N; i++) 
+                sum += *(arr + i);
+
+            cout << "sum is " << sum << endl;
+        };
+
+        void get_min(){
+            T min = *arr;
+
+            for(int i = 0; i < N; i++) 
+                if(min > *(arr + i)) min = *(arr + i);
+
+            cout << "min is " << min << endl;
+        };
+
+        void get_max(){
+            T max = *arr;
+
+            for(int i = 0; i < N; i++) 
+                if(*(arr + i) > max) max = *(arr + i);
+
+            cout << "max is " << max << endl;
+        };
+
+        ~Arrays(){ delete[] arr; cout << "delete" << endl;};
+
+};
+
 
 int main() {
 
-    int arr[] = {1,2,3,4,56,5};
-    float arr2[] = {1.4f,2.2f,3.55f,4.1f,56.01f,5};
-    string arr3[] = {"1.4f","2","3","4","56","5"};
-    char arr4[] = {'1', 'd', 'h', 'H', '0'};
-    bool arr5[] = {true, false, false, false};
+    int array[] = {1,2,3,4,5,6,7,8,9,0};
+    float array2[] = {1.1,2,3,4,5,6,7.2,8,9,0.5f};
 
 
-    show_all<int, int>(arr);
-    show_all<float, int>(arr2);
-    show_all<string, int>(arr3);
-    // show_all(arr4);
-    // show_all(arr5);
+    Arrays test(array); 
+
+    test.get_all();
+
+    Arrays test2(array2); 
+
+    test2.get_all();
 
 
     return 0;
