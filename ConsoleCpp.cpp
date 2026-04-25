90a343ae844e1df413ea5f097acdec8b85e72755
Kostia <kozzztia@gmail.com>
2026-04-25 12:26:46 +0300

    extendet : public  class

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -1,60 +1,104 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 using namespace std;
 
-class Car; // предварительное объявление
 class Person;
+class User;
 
-class Car {
-    private:
+class Person{
+    protected : 
         string name;
-        float speed;
+        int year;
+        bool smart;
+        int  age;
 
-    public:
-        Car(const string& name, float speed) : name(name), speed(speed) {}
-
-        void info() const {
-            cout << "Car: " << this->name << ", speed: " << this->speed << endl;
+        int get_year(){
+            time_t now = time(0);
+            tm* localTime = localtime(&now); 
+            return 1900 + localTime->tm_year;
         }
 
-        // Разрешаем Person доступ к приватным полям Car
-        friend class Person;
-};
-
-class Person {
-    private:
-        string name;
-
+        void create_info(){
+            cout << "create person :" << this->name << endl; 
+        }
+        void delete_info(){
+            cout << "delete person :" << this->name << endl; 
+        }
+    
     public:
-        Person(const string& name) : name(name) {}
+        Person(const string &name, int year): name(name), year(year){
 
-        void fast(Car& car){
-            car.speed += 60;
-            cout << car.speed << endl;
+            this->age = get_year() - year;
+            this->smart = age < 20;
+            create_info();
         };
-        void slow(Car& car){
-            car.speed -= 60;
-            cout << car.speed << endl;
-        };
-        void stop(Car& car){
-            car.speed = 0;
-            cout << car.speed << endl;
-        };
+
+        Person() : name("Unknown"), year(2000), smart(false), age(0) {create_info();}
+        
+        ~Person(){delete_info();}
+
+        void get_info(){
+            string info = this-> name + " : " + to_string(this->age) + " years old, so hi is " + (this->smart? "smart": "fool");
+            cout << info;
+        }
+
 };
+
+class User : public Person{
+    protected: 
+        string pc;
+    public:
+        User(const string& name, int year, const string& pc): Person(name, year), pc(pc) {} //данные  пошли  в  родительский  класс + пс новое
+        void get_info(){
+            Person::get_info();
+            cout << " has  pc: " << this->pc << ' ';
+        }
+        void get_pc_info() {
+            cout << "User " << this->name << " has PC: " << this->pc << endl;
+        } 
+    
+};
+
+class Worker : public User{
+    protected: 
+        string work;
+    public:
+        Worker(const string& name, int year,const string& pc, const string& work): User(name, year , pc), work(work) {} //данные  пошли  в  родительский  класс + work новое
+        void get_info(){
+            Person::get_info();
+            cout << " has  work: " << this->work << ' ';
+        }
+        void get_pc_info() {
+            cout << "Worker " << this->name << " has PC: " << this->pc << endl;
+        } 
+
+        void get_work_info() {
+            cout << "Worker " << this->name << " has work: " << this->work << endl;
+        } 
+    
+};
+
 
 int main() {
-    Person ivan("Ivan");
-    Car bmw("BMW", 1000);
 
-    bmw.info();
-    ivan.fast(bmw);
-    ivan.fast(bmw);
-    ivan.fast(bmw);
+    Person misha("Misha", 1990);
 
-    ivan.slow(bmw);
+    misha.get_info();
 
-    ivan.stop(bmw);
-    bmw.info();
+    User ivan("ivan", 2015, "hp");
+
+    ivan.get_info();
+
+    ivan.get_pc_info();
+
+    Worker kostia("Kostia", 1987, "hp", "programmer");
+
+    kostia.get_info();
+
+    kostia.get_pc_info();
+
+    kostia.get_work_info();
 
     return 0;
 }
